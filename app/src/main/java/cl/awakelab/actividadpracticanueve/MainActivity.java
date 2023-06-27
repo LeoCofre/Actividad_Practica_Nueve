26433380a4e5cebbb7794d0a938000cc9d6e6000
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-06-27 02:35:42 -0400

    Paso de primera a segunda activity desde las imagenes.

--- app/src/main/java/cl/awakelab/actividadpracticanueve/MainActivity.java
+++ app/src/main/java/cl/awakelab/actividadpracticanueve/MainActivity.java
@@ -2,7 +2,11 @@ package cl.awakelab.actividadpracticanueve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+        ImageButton botonUno = findViewById(R.id.button1);
+        ImageButton botonDos = findViewById(R.id.button2);
+        ImageButton botonTres = findViewById(R.id.button3);
+        ImageButton botonCuatro = findViewById(R.id.button4);
+
+        botonUno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSecondActivity();
+
+            }
+        });
+        botonDos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSecondActivity();
+
+            }
+        });
+        botonTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSecondActivity();
+
+            }
+        });
+        botonCuatro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSecondActivity();
+
+            }
+        });
+    }
+
+    public void changeSecondActivity() {
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 }
